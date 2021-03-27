be1a17acfc43da84f6b3cc7f81e564d3e63319e2
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2021-03-27 12:47:54 -0400

    routing and pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,23 @@ import React from 'react';
 // apollo boost library statements 
 //ApolloProvider, is a special type of React component that we'll use to provide data to all of the other components. We'll use the second, ApolloClient, to get that data when we're ready to use it.
 import { ApolloProvider } from '@apollo/react-hooks';
+
 import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
+
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
+
+//BrowserRouter and Route are components that the React Router library provides. We renamed BrowserRouter to Router to make it easier to work with.
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 //With this code, we establish a new connection to the GraphQL server using Apollo. We could pass many other options and configuration settings into this constructor function. We'll use another one down the road, but for now this is all we need.
@@ -20,15 +31,28 @@ const client = new ApolloClient({
 
 function App() {
   //Note how we wrap the entire returning JSX code with <ApolloProvider>. Because we're passing the client variable in as the value for the client prop in the provider, everything between the JSX tags will eventually have access to the server's API data through the client we set up
+
+  //A few things have changed. We've wrapped the <div className="flex-column"> element in a Router component, which makes all of the child components on the page aware of the client-side routing that can take place now.
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              {/*The ? means this parameter is optional, so /profile and /profile/myUsername will both render the Profile component. Later on, we'll set up /profile to display the logged-in user's information*/}
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component= {NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 
-  //Here we instruct that the ThoughtList component will receive two props: a title and the thoughts array. We destructure the argument data to avoid using props.title and props.thoughts throughout the JSX code.
+  //Here we instruct that the ThoughtList component will receive two props: a title and the thoughts array being render from the profile component in Profile.js and also from the hompage component (the props mean different things depending on where the thoughlist is being rendered)... 
 const ThoughtList = ({ thoughts, title }) => {
 
     //We conditionally render JSX by checking to see if there's even any data in the thoughts array first. If there's no data, then we return a message stating that. If there is data, then we return a list of thoughts using the .map() method
@@ -21,20 +22,28 @@ const ThoughtList = ({ thoughts, title }) => {
             thoughts.map(thought => (
                 <div key={thought._id} className="card mb-3">
                     <p className="card-header">
-                        {thought.username}
-                        thought on {thought.createdAt}
+                        <Link
+                            to={`/profile/${thought.username}`}
+                            style= {{ fontWeight: 700 }}
+                            className = "text-light"
+                        >
+                            {thought.username}
+                        </Link> {''}
+                            thought on {thought.createdAt}
                     </p>
                     <div className="card-body">
-                        <p>{thought.thoughtText}</p>
-                        <p className="mb-0">
-                        {/* conditional/ ternary operator If condition is true, the operator has the value of val1. Otherwise it has the value of val2. You can use the conditional operator anywhere you would use a standard operator.
-                        
-                        Notice how we also check to see the value of thought.reactionCount. We're conditionally displaying a message to contextualize what the call to action should be. If there are no reactions, the user will start the discussion by adding the first reaction. If there are reactions, the user will view or add their own reaction to an existing list.
-                        
-                        */}
-                        Reactions: {thought.reactionCount} || Click to{' '}
-                        {thought.reactionCount ? 'see' : 'start'} the discussion!
-                        </p>
+                        <Link to={`/thought/${thought._id}`}>
+                            <p>{thought.thoughtText}</p>
+                            <p className="mb-0">
+                            {/* conditional/ ternary operator If condition is true, the operator has the value of val1. Otherwise it has the value of val2. You can use the conditional operator anywhere you would use a standard operator.
+                            
+                            Notice how we also check to see the value of thought.reactionCount. We're conditionally displaying a message to contextualize what the call to action should be. If there are no reactions, the user will start the discussion by adding the first reaction. If there are reactions, the user will view or add their own reaction to an existing list.
+                            
+                            */}
+                            Reactions: {thought.reactionCount} || Click to{' '}
+                            {thought.reactionCount ? 'see' : 'start'} the discussion!
+                            </p>
+                        </Link>
                     </div>
                 </div>
             ))}
